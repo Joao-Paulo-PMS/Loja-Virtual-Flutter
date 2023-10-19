650769967dd5a1ca9e84b735d3b5cd694a66f749
JoãoPPMSANTOS <joaopaulopaiva1@hotmail.com>
2023-10-19 16:48:02 -0300

    Revert "versão final"
    This reverts commit 86c290e8761688b8976136127690a52571786466.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:loja_virtual_b/views/Anuncios.dart';
 import 'package:loja_virtual_b/views/home.dart';
 import 'dart:async';
-import 'package:loja_virtual_b/views/login.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:loja_virtual_b/RouteGenerator.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:loja_virtual_b/views/Login.dart';
-import 'package:loja_virtual_b/views/MeusAnuncios.dart';
-import 'package:loja_virtual_b/views/NovoAnuncio.dart';
+
 
 
 final ThemeData temaPadrao = ThemeData(
@@ -23,29 +17,9 @@ void main() async {
     await Firebase.initializeApp();
     runApp(MaterialApp(
       title: "Loja virtual",
-      home: Anuncios(),
+      home: Home(),
       theme: temaPadrao,
-      onGenerateRoute: RouteGenerator.generateRoute,
       debugShowCheckedModeBanner: false,
-
     ));
-    child: Image.asset(
-      "imagens/bicicleta.jpg",
-      fit: BoxFit.fitWidth,
-    );
-    child: Image.asset(
-      "imagens/carro.jpg.jpg",
-      fit: BoxFit.fitWidth,
-    );
-    child: Image.asset(
-      "imagens/tv.jpg",
-      fit: BoxFit.fitWidth,
-    );
+
 }
-//  Image.asset('imagens/bicicleta.jpg');
-//  Image.asset('imagens/carro.jpg');
-//Image.asset('imagens/tv.jpg');
-/*
-      * contain, cover, fill, fitHeight,
-      * fitWidth, none, scaleDown
-      * */
